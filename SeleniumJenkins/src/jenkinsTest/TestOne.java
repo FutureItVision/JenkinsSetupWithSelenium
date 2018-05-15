6c53511290ade353697f30e7860b8c74a456f511
nadiruzzaman <nadiruzzamanqa@gmail.com>
2018-05-15 01:01:18 -0400

    this is jenkins

---
+++ SeleniumJenkins/src/jenkinsTest/TestOne.java
@@ -0,0 +1,20 @@
+package jenkinsTest;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestOne {
+	
+	
+	@Test
+	public void firstJenkinTest(){
+		
+	System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+	ChromeDriver driver = new ChromeDriver();
+	driver.get("https://www.groupm.com/");
+	driver.findElementByXPath(".//*[@id='navbar']/div/div/div/a/img").isDisplayed();
+	driver.close();
+				
+	}
+
+}
